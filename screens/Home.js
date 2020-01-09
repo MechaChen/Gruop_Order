d4982fa975264c4524a62fe50572508a37055ab8
Benson Chen <tumlivein@gmail.com>
2020-01-09 17:37:33 +0800

    add Home screen View and Functions

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,87 @@
 import React, { useState } from 'react';
 import { View, Text, FlatList } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 import { globalStyles } from '../styles/global';
 
 export default function Home() {
+    const [shop, setShop] = useState('大方雞腿王');
+    const [date, setDate] = useState('1/7');
+    const [order, setOrder] = useState([
+        { key: '1', paid: false, name: '俊偉', item: '香滷大雞腿', price: 80 },
+        { key: '2', paid: false, name: 'FM', item: '蜜汁大雞腿', price: 80 },
+        { key: '3', paid: false, name: 'CY', item: '蜜汁大雞腿', price: 80 },
+        { key: '4', paid: false, name: '余光', item: '香滷大雞腿', price: 80 },
+        { key: '5', paid: false, name: '敬岳', item: '蜜汁大排骨', price: 80 },
+        { key: '6', paid: false, name: '貴麟', item: '宮保雞丁', price: 70 },
+        { key: '7', paid: false, name: 'Link', item: '酥炸大排骨', price: 80 },
+        { key: '8', paid: false, name: '柏緯', item: '酥炸大排骨', price: 80 },
+    ]);
+
+    const checkItem = (key) => {
+        setOrder(order.map((item) => item.key === key ? { ...item, paid: !item.paid } : item ));
+    }
+
+    const total = order.reduce((accum, cur) => {
+        return accum + cur.price
+    }, 0);
+
+    const paidTotal = order.reduce((accum, cur) => {
+        return accum + (cur.paid ? cur.price : 0);
+    }, 0);
+
+    const people = order.reduce((accum, cur) => {
+        return accum + 1;
+    }, 0);
+
+    const paidPeople = order.reduce((accum, cur) => {
+        return accum + (cur.paid ? 1 : 0);
+    }, 0);
+
+    const sortedOrder = () => {
+        const paidItems = [];
+        const unPaidItems = [];
+        order.forEach((item) => {
+            item.paid ? paidItems.push(item) : unPaidItems.push(item);
+        })
+        return [...unPaidItems, ...paidItems];
+    }
+
     return (
         <View style={globalStyles.container}>
-            <Text>Home page</Text>
+            <Text style={globalStyles.infoText}>店家：{shop}</Text>
+            <Text style={globalStyles.infoText}>日期：{date}</Text>
+            <View style={globalStyles.itemTitle}>
+                <Text style={globalStyles.titleText}>Paid</Text>
+                <Text style={globalStyles.titleText}>Name</Text>
+                <Text style={globalStyles.titleText}>Item</Text>
+                <Text style={globalStyles.titleText}>Price</Text>
+            </View>
+            <FlatList
+                style={globalStyles.items}
+                data={sortedOrder()}
+                renderItem={({ item }) => (
+                    <View style={globalStyles.itemContainer}>
+                        <CheckBox
+                            onPress={() => checkItem(item.key)}
+                            checked={item.paid}
+                            checkedColor="coral"
+                        />
+                        <Text style={globalStyles.itemText}>{item.name}</Text>
+                        <Text style={globalStyles.itemText}>{item.item}</Text>
+                        <Text style={globalStyles.itemText}>{item.price}</Text>
+                    </View>
+                ) }
+            />
+            <View style={globalStyles.status}>
+                <View>
+                    <Text style={globalStyles.statusText}>已付金額 / 總金額</Text>
+                    <Text style={[globalStyles.statusText, globalStyles.statusNumber]}>{paidTotal} / {total}</Text>
+                </View>
+                <View>
+                    <Text style={globalStyles.statusText}>已付款人數 / 總人數</Text>
+                    <Text style={[globalStyles.statusText, globalStyles.statusNumber]}>{paidPeople} / {people}</Text>
+                </View>
+            </View>
         </View>
     );
 }
--- styles/global.js
+++ styles/global.js
@@ -2,23 +2,55 @@ import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
     container: {
-        padding: 24,
+        paddingVertical: 30,
+        paddingHorizontal: 40,
     },
-    itemContainer: {
-        marginVertical: 20,
+    infoText: {
+        paddingVertical: 5,
+        fontSize: 14,
+        color: '#333',
+    },
+    itemTitle: {
+        marginTop: 30,
+        marginVertical: 5,
         paddingVertical: 10,
+        paddingHorizontal: 20,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        backgroundColor: 'coral',
+        borderRadius: 5,
+    },
+    titleText: {
+        color: '#fff',
+        fontSize: 20,
+    },
+    items: {
+        height: 380
+    },
+    itemContainer: {
+        marginVertical: 10,
+        // paddingVertical: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         borderBottomWidth: 1,
         borderBottomColor: '#333',
     },
-    titleText: {
-        fontFamily: 'nunito-bold',
-        fontSize: 18,
-        color: '#333'
+    itemText: {
+        fontSize: 16,
     },
-    paragraph: {
-        marginVertical: 8,
-        lineHeight: 20,
+    status: {
+        paddingTop: 35,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    statusText: {
+        textAlign: 'center',
+        paddingBottom: 10,
+        fontSize: 16,
+    },
+    statusNumber: {
+        fontSize: 24,
     }
 });
